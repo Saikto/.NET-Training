bd17ec91ba1e59901a84b2990cb76e9486bd4bb1
Igor Neslukhovski <igor_neslukhovski@epam.com>
2017-09-18 15:00:32 +0300

    Add static class, input, output

--- ConsoleProject/ConsoleProject/Program.cs
+++ ConsoleProject/ConsoleProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryProject;
 
 namespace ConsoleProject
 {
@@ -10,10 +11,11 @@ namespace ConsoleProject
     {
         static void Main(string[] args)
         {
-            var a = 0;
-            var b = 0;
+            int a = 0;
+            int b = 0;
             bool flagForA = true;
             bool flagForB = true;
+            Console.WriteLine("Enter 2 numbers:");
             if (!Int32.TryParse(Console.ReadLine(), out a))
             {
                 flagForA = false;
@@ -26,7 +28,14 @@ namespace ConsoleProject
             }
             if (flagForA && flagForB)
             {
-                Console.WriteLine(a+b);
+                Console.Write("Sum: ");
+                Console.WriteLine(ArifmeticOperations.NumbersSum(a, b));
+                Console.Write("Substraction: ");
+                Console.WriteLine(ArifmeticOperations.NumbersSubtraction(a, b));
+                Console.Write("Division: ");
+                Console.WriteLine(ArifmeticOperations.NumbersDivision(a, b));
+                Console.Write("Multiplication: ");
+                Console.WriteLine(ArifmeticOperations.NumbersMultiplication(a, b));
             }
         }
     }
---
+++ ConsoleProject/LibraryProject/ArifmeticOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject
+{
+    public static class ArifmeticOperations
+    {
+
+        public static int NumbersSum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int NumbersSubtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static double NumbersDivision(double a, double b)
+        {
+            return (a / b);
+        }
+        public static double NumbersMultiplication(int a, int b)
+        {
+            return (a * b);
+        }
+    }
+}
