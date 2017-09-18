1a8612d8b4d3149cc1ae4e0cb01f65bdf03345db
Igor Neslukhovski <igor_neslukhovski@epam.com>
2017-09-18 14:35:16 +0300

    Upload project

---
+++ ConsoleProject/ConsoleProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 0;
+            var b = 0;
+            bool flagForA = true;
+            bool flagForB = true;
+            if (!Int32.TryParse(Console.ReadLine(), out a))
+            {
+                flagForA = false;
+                Console.WriteLine("First value is not a number!");
+            }
+            if (!Int32.TryParse(Console.ReadLine(), out b))
+            {
+                flagForB = false;
+                Console.WriteLine("Second value is not a number!");
+            }
+            if (flagForA && flagForB)
+            {
+                Console.WriteLine(a+b);
+            }
+        }
+    }
+}
